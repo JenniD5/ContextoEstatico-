2b42efb68d3ea6073040a744673b740f503bc3a5
JenniD5 <elizabethduran0105@gmail.com>
2019-08-30 08:42:51 -0700

    Agregar archivos de proyecto.

---
+++ ContextoEstatico/Matematicas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Matematicas
+    {
+        public const double PI = 3.14159;
+        //variable estatica, significa que se puede tener acceso a ella solo a travez de la clase 
+        //el const lo hace static y no se permite cambiar su contenido y/o valor 
+        public static double sumar(double sumando1, double sumando2) //con el static, no es necesario instanciar la clase 
+        {
+            return sumando1 + sumando2;
+        }
+
+        public static double areaCirculo(double radio)
+        //double es el tipo de dato que va regresar 
+
+        {
+            return (radio * radio) * PI;
+        }
+
+
+    }
+}
---
+++ ContextoEstatico/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Program
+    {
+        static void Main(string[] args) //las variables no tienen acceso a metodos estaticos 
+        {
+            
+            double sumando1 = 10;
+            double sumando2 = 20;
+            double resultado = Matematicas.sumar(sumando1, sumando2);// se debe mandar llamar ala clase, ya que no existe ninguna variable
+            //al llamar a la clase, esta se ejecuta 
+
+            Console.WriteLine(sumando1.ToString() +"+" + sumando2.ToString() + "=" + resultado.ToString());
+
+            double radio = 6.7;
+            Console.WriteLine("radio: " + radio.ToString() + " area= " + Matematicas.areaCirculo(radio));//hacerlo mas dinamico, declarando antes la varibale a la que va ser igual el radio
+            Console.WriteLine("radio: 5, area es = " + Matematicas.areaCirculo (4.7));
+            // esta linea no sirve si se pone conts en la linea 11 de la clase "Matematicas.PI = 95.36;"
+            //Nuevo valor de Pi en toda la aplicacion, a que solo hay un espacio de memoria para pi
+
+
+            Console.Read();//siempre debe ir al final, si no no se ejecutara lo que quede debajo de el 
+        }
+    }
+}
